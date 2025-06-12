d9454ac1e5fc0c8eedfdcc5639675d4829131e47
Martian-maker <rohangharate@gmail.com>
2025-06-12 00:34:13 -0700

    Update app.py

--- data/app.py
+++ data/app.py
@@ -1,6 +1,5 @@
-import streamlit as st
-
 st.set_page_config(page_title="Exoplanet Habitability Dashboard", layout="wide")
+import streamlit as st
 
 import pandas as pd
 import matplotlib.pyplot as plt
