e59bd3991d5449ade33a4346327248b141947a56
Martian-maker <rohangharate@gmail.com>
2025-06-11 22:46:20 -0700

    Update app.py

--- data/app.py
+++ data/app.py
@@ -4,6 +4,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+from ingest import download_nasa_data
+from preprocess import process_exoplanets  # you can define this
+
+# Step 1: Download fresh data
+raw_csv_path = download_nasa_data()
+
+# Step 2: Preprocess it (returns DataFrame)
+df = process_exoplanets(raw_csv_path)
+
+
 dir_path = os.path.dirname(os.path.abspath(__file__))
 csv_path = os.path.join(dir_path, "ranked_planets.csv")
 
