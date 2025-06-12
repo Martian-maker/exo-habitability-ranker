78b710d7dcacc37c7d38df339c9a87d62c2421ff
Martian-maker <rohangharate@gmail.com>
2025-06-12 14:09:03 -0700

    Update app.py

--- data/app.py
+++ data/app.py
@@ -14,6 +14,7 @@ def get_processed_data():
     csv_path = download_nasa_data()
     return process_exoplanets(csv_path)
 
+df = get_processed_data()
 
 # === Header ===
 st.title("🪐 Exoplanet Habitability Dashboard")
