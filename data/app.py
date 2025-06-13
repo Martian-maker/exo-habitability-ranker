2df7f45b571f293ffbe64edefd01073011202014
Martian-maker <rohangharate@gmail.com>
2025-06-12 22:35:55 -0700

    Update app.py

--- data/app.py
+++ data/app.py
@@ -5,14 +5,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-# force cache clear (DEV only)
-st.cache_data.clear()
-
 
 from ingest import download_nasa_data
 from preprocess import process_exoplanets
 
-@st.cache_data
+- @st.cache_data
 def get_processed_data():
     csv_path = download_nasa_data()
     return process_exoplanets(csv_path)
